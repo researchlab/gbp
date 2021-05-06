bcead416ade4269b9bdf8426207c018ca19997be
hong li <lihongitchina@gmail.com>
2021-05-06 18:03:53 +0800

    http server

--- debug/03-dlv-http-server.go
+++ debug/03-dlv-http-server.go
@@ -4,14 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/hi", hi)
+	greet := "Hello World!"
+	fmt.Println("greet:", greet)
+	func01()
+	func02()
+	http.HandleFunc("/sum", sum)
+	fmt.Println("running on port:8081")
 	http.ListenAndServe(":8081", nil)
 }
 
+func func01() {
+	fmt.Println("func01")
+}
+
+func func02() {
+	fmt.Println("func02")
+}
+
+func sum(w http.ResponseWriter, r *http.Request) {
+	result := 0
+	if r.Method == "GET" {
+		vals := r.URL.Query()
+		_num := vals.Get("num")
+		if len(_num) != 0 {
+			num, _ := strconv.Atoi(_num)
+			if num != 0 {
+				for i := 0; i <= num; i++ {
+					result += i
+				}
+			}
+		}
+	}
+	fmt.Fprintf(w, "result: %d\n", result)
+}
 func hi(w http.ResponseWriter, r *http.Request) {
+	doHi(w)
+}
+
+func doHi(w http.ResponseWriter) {
 	hostName, _ := os.Hostname()
-	fmt.Fprintf(w, "Hostname: %s", hostName)
+	fmt.Fprintf(w, "Hostname: %s\n", hostName)
 }
