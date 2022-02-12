b44dcdb6676c1877a8877332246f223098daf5a5
hong li <lihongitchina@gmail.com>
2022-02-12 22:05:21 +0800

    update

--- adv/03-copy.go
+++ adv/03-copy.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
-
-	"github.com/researchlab/gbp/adv/utils"
 )
 
 // golang 中结构体的深拷贝和浅拷贝
 
 type User struct {
 	Name string
+}
+
+// common
+// DeepCopy copy
+func DeepCopy(dst, src interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
 }
 
 func main() {
@@ -35,7 +45,18 @@ func main() {
 	}
 	user6 := new(User)
 	fmt.Printf("before user5:%v, user6:%v\n", user5, user6)
-	utils.DeepCopy(user6, user5)
+	DeepCopy(user6, user5)
+	user6.Name = "update"
 	fmt.Printf("after user5:%v, user6:%v\n", user5, user6)
+
+	fmt.Println("值类型, DeepCopy深拷贝:")
+	user7 := User{
+		Name: "why",
+	}
+	user8 := new(User)
+	fmt.Printf("before user7:%v, user8:%v\n", user7, user8)
+	DeepCopy(user8, user7)
+	user8.Name = "update"
+	fmt.Printf("after user7:%v, user8:%v\n", user7, user8)
 
 }
