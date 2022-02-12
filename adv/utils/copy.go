d3c21d60430d09f67c9088ef1168a0b619d0f7e2
hong li <lihongitchina@gmail.com>
2022-02-12 21:44:58 +0800

    update

---
+++ adv/utils/copy.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// DeepCopy copy
+func DeepCopy(dst, src interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
+}
