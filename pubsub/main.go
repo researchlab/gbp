3954d36a5cd3ef060438876bafe5f48e98223437
hong li <lihongitchina@gmail.com>
2022-06-08 13:38:21 +0800

    pubsub

---
+++ pubsub/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	p := NewPublisher(100*time.Millisecond, 10)
+	defer p.Close()
+
+	all := p.Subscribe()
+	golang := p.SubscribeTopic(func(v interface{}) bool {
+		if s, ok := v.(string); ok {
+			return strings.Contains(s, "golang")
+		}
+		return false
+	})
+
+	p.Publish("Hello, world!")
+	p.Publish("Hello, golang")
+
+	go func() {
+		for msg := range all {
+			fmt.Println("all:", msg)
+		}
+	}()
+	go func() {
+		for msg := range golang {
+			fmt.Println("golang:", msg)
+		}
+	}()
+
+	// 运行一定时间后退出
+	time.Sleep(3 * time.Second)
+}
