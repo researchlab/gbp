6ca8c487db69a26cb315406fabf3420304101e6e
hong li <lihongitchina@gmail.com>
2023-01-09 16:55:14 +0800

    update

--- rand/scope_min_max_rand_float64_array.go
+++ rand/scope_min_max_rand_float64_array.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/big"
 	"math/rand"
 	"time"
+	RR "crypto/rand"
 )
 
 func randFloats(min, max float64, n int) []float64 {
@@ -14,8 +17,24 @@ func randFloats(min, max float64, n int) []float64 {
 	return res
 }
 
+// 生成区间[-m, n]的安全随机数
+func RangeRand(min, max int64) int64 {
+	if min > max {
+		panic("the min is greater than max")
+	}
+	if min < 0 {
+		f64Min := math.Abs(float64(min))
+		i64Min := int64(f64Min)
+		result, _ := RR.Int(RR.Reader, big.NewInt(max+1+i64Min))
+		return result.Int64() - i64Min
+	} else {
+		result, _ := RR.Int(RR.Reader, big.NewInt(max-min+1))
+		return min + result.Int64()
+	}
+}
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println(randFloats(1.10, 101.99, 5))
 	fmt.Println(randFloats(1.10, 101.99, 5))
+	fmt.Println(RangeRand(-100, 100))
 }
